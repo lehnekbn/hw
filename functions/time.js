1097373fa24844e3c70a8278f94b85a8276b6d97
lehnekbn <karlheinz.lehner@rub.de>
2026-04-20 18:56:14 +0200

    Fix response headers syntax in onRequest function

--- functions/time.js
+++ functions/time.js
@@ -2,9 +2,7 @@
 export async function onRequest(context) {
   const currentTime = new Date().toLocaleTimeString();
   
-  return new Response(
-    "XXX"+JSON.stringify({ time: currentTime }),
-    (headers: { "Content-Type": "application/json" }
-  );
+  return new Response("XXX"+JSON.stringify({ time: currentTime }), {
+    headers: { "Content-Type": "application/json" }
+  });
 }
-
