da4b1870aa4916130345ce5fcbdca01b34638f68
Karlheinz Lehner <karlheinz.lehner@rub.de>
2026-04-20 16:10:12 +0200

    all files

---
+++ functions/time.js
@@ -0,0 +1,9 @@
+// This function runs on the server whenever someone visits /time
+export async function onRequest(context) {
+  const currentTime = new Date().toLocaleTimeString();
+  
+  return new Response(JSON.stringify({ time: currentTime }), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
+
